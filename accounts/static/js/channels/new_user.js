71e6969dcd6e4cf0d8209a5e6f1c065610c517ab
Muhd Rahiman <50654608+mdrhmn@users.noreply.github.com>
2021-10-13 02:01:04 +0800

    Removed commented jQuery code for web socket configuration

--- accounts/static/js/channels/new_user.js
+++ accounts/static/js/channels/new_user.js
@@ -1,34 +1,3 @@
-// $(function () {
-//     endpoint = 'ws://' +
-//         window.location.host +
-//         '/ws/new-user/'
-
-//     // 1. Use ReconnectingWebSocket (auto-reconnect upon disconnect)
-//     var socket = new ReconnectingWebSocket(endpoint) // 2
-//     // 2. Use default WebSocket
-//     // var socket = new WebSocket(endpoint) // 2
-
-//     // 3
-//     socket.onopen = function (e) {
-//         console.log("open", e);
-//     }
-
-//     socket.onerror = function (e) {
-//         console.log("error", e)
-//     }
-
-//     socket.onclose = function (e) {
-//         console.log("close", e)
-//     }
-
-//     socket.onmessage = function (e) {
-//         console.log("message", e)
-//         var userData = JSON.parse(e.data)
-//         $('#new_user').html(userData.html_users)
-//     }
-// });
-
-
 $(document).ready(function () {
 
     endpoint = 'ws://' +
