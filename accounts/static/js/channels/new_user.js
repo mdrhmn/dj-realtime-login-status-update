c1d57b7af243483844ba02ed2f6bbb4fe020c8f1
Muhd Rahiman <50654608+mdrhmn@users.noreply.github.com>
2021-10-13 02:00:31 +0800

    Added vanilla JS code equivalent for web socket configuration

--- accounts/static/js/channels/new_user.js
+++ accounts/static/js/channels/new_user.js
@@ -1,28 +1,62 @@
-$(function () {
+// $(function () {
+//     endpoint = 'ws://' +
+//         window.location.host +
+//         '/ws/new-user/'
+
+//     // 1. Use ReconnectingWebSocket (auto-reconnect upon disconnect)
+//     var socket = new ReconnectingWebSocket(endpoint) // 2
+//     // 2. Use default WebSocket
+//     // var socket = new WebSocket(endpoint) // 2
+
+//     // 3
+//     socket.onopen = function (e) {
+//         console.log("open", e);
+//     }
+
+//     socket.onerror = function (e) {
+//         console.log("error", e)
+//     }
+
+//     socket.onclose = function (e) {
+//         console.log("close", e)
+//     }
+
+//     socket.onmessage = function (e) {
+//         console.log("message", e)
+//         var userData = JSON.parse(e.data)
+//         $('#new_user').html(userData.html_users)
+//     }
+// });
+
+
+$(document).ready(function () {
+
     endpoint = 'ws://' +
         window.location.host +
         '/ws/new-user/'
-    '/'
-    
-    var socket = new ReconnectingWebSocket(endpoint) // 2
+
+    // 1. Use ReconnectingWebSocket (auto-reconnect upon disconnect)
+    const socket = new ReconnectingWebSocket(endpoint) // 2
+    // 2. Use default WebSocket
+    // const socket = new WebSocket(endpoint) // 2
 
     // 3
     socket.onopen = function (e) {
         console.log("open", e);
     }
-    socket.onmessage = function (e) {
-        console.log("message", e)
-    }
+
     socket.onerror = function (e) {
         console.log("error", e)
     }
+
     socket.onclose = function (e) {
         console.log("close", e)
     }
 
     socket.onmessage = function (e) {
         console.log("message", e)
-        var userData = JSON.parse(e.data)
-        $('#new_user').html(userData.html_users)
+        const userData = JSON.parse(e.data)
+        document.querySelector('#new_user').innerHTML = userData.html_users
     }
+
 });
